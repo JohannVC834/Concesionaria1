071c92d662b49d2d8126a8a438af756ac4354957
Johann <johann12032005@gmail.com>
2024-11-13 14:18:09 -0600

    Implementacion de la conexión a la base de datos creada para el proyecto, mediante ODBC

--- app.py
+++ app.py
@@ -13,3 +13,18 @@ login_manager.login_view = 'login'
 
 ADMIN_USERNAME = "admin"
 ADMIN_PASSWORD = "admin123" 
+
+class Usuario(UserMixin):
+    def __init__(self, id, nombre, is_admin=False):
+        self.id = id
+        self.nombre = nombre
+        self.is_admin = is_admin
+        
+def conectar():
+    try:
+        conn = pyodbc.connect(f"DSN={DSN_NAME};UID={DB_USER};PWD={DB_PASSWORD}")
+        print("Conexión a la base de datos exitosa.")
+        return conn
+    except pyodbc.Error as e:
+        print("Error al conectar a la base de datos:", e)
+        return None
